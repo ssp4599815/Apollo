e5b17bfe25ec129be0be65e33c44d717cbb0426a
root <root@localhost.localdomain>
2025-08-03 05:07:24 +0800

    update

--- fuliji/fuliji/spiders/chigua.py
+++ fuliji/fuliji/spiders/chigua.py
@@ -20,10 +20,10 @@ class ChiguaSpider(SpiderLoggerMixin, scrapy.Spider):
         self.setup_spider_logger()
 
     def start_requests(self):
-        for page in range(5, 15):
+        for page in range(15, 35):
             for tag in ["萝莉", '一线天', '馒头逼', '福利姬', '自慰',
                         '美乳', '粉嫩', '学生妹', '鲍鱼', '嫩穴', '小穴',
-                        '大学', '高中', '海角']:
+                        '大学', '高中']:
                 yield Request(url=f'https://cabinet.byoeyvro.club/search/{tag}/{page}/')
 
     def parse(self, response):
