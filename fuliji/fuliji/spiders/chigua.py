7c6ff6dd7d319b7c0933a3063a050fd0ec3d03a8
root <root@localhost.localdomain>
2025-08-04 11:37:14 +0800

    xx

--- fuliji/fuliji/spiders/chigua.py
+++ fuliji/fuliji/spiders/chigua.py
@@ -21,7 +21,7 @@ class ChiguaSpider(SpiderLoggerMixin, scrapy.Spider):
         self.setup_spider_logger()
 
     def start_requests(self):
-        for page in range(5, 25):
+        for page in range(25, 26):
             for tag in ["萝莉", '一线天', '馒头逼', '福利姬', '自慰',
                        '美乳', '粉嫩', '学生妹', '鲍鱼', '嫩穴', '小穴',
                        '大学', '高中']:
